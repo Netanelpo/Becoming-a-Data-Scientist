2da2376c0957270c81a84efdb29e95657b427577
Netanel Polonsky <netanel.polonsky@tufin.com>
2023-06-04 12:52:10 +0300

    first plots

--- main.py
+++ main.py
@@ -2,11 +2,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    df = pd.read_csv('BTC-USD.csv')
-    assert df.shape == (3183,7), df.shape
-    columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
-    assert df.columns.equals(pd.Index(columns)), df.columns
+    btc = pd.read_csv('BTC-USD.csv')
+    assert btc.shape == (3183, 7), btc.shape
+    btc_columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+    assert btc.columns.equals(pd.Index(btc_columns)), btc.columns
 
-    btc = df[['Date', 'Close']]
+    m2_US = pd.read_csv('US_M2.csv')
+    assert m2_US.shape == (539, 15), m2_US.shape
+    m2_columns = ['time', 'open', 'high', 'low', 'close', 'Global Money Supply', 'Volume',
+                  'Volume MA', 'RSI', 'RSI-based MA', 'Upper Bollinger Band',
+                  'Lower Bollinger Band', 'Predicted Funding Rate', 'Next Funding Rate', 'Funding Bar']
+    assert m2_US.columns.equals(pd.Index(m2_columns)), m2_US.columns
+
+    btc = btc[['Date', 'Close']]
+    btc['Date'] = pd.to_datetime(btc['Date'])
+    btc.set_index('Date', inplace=True)
+    assert btc.index[0] == pd.Timestamp('2014-09-17'), btc.index[0]
+    assert btc.iloc[0].item() == 457.334015, btc.iloc[0].item()
+
+    m2_US = m2_US[['time', 'close']]
+    m2_US['time'] = pd.to_datetime(m2_US['time'])
+    m2_US.set_index('time', inplace=True)
+    assert m2_US.index[0] == pd.Timestamp('1978-06-01 00:00:00+00:00'), m2_US.index[0]
+    assert m2_US.iloc[0].item() == 1318500000000, m2_US.iloc[0].item()
+
     btc.plot()
+    m2_US.plot()
     plt.show()
