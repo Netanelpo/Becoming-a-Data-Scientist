7773aacb55c0d52cc4918f741b756a429e685813
Netanel Polonsky <netanel.polonsky@tufin.com>
2023-06-05 10:32:20 +0300

    adding nasdaq + correlation

--- main.py
+++ main.py
@@ -1,33 +1,73 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as sp
+
+columns = ['time', 'open', 'high', 'low', 'close', 'Global Money Supply', 'Volume',
+           'Volume MA', 'RSI', 'RSI-based MA', 'Upper Bollinger Band',
+           'Lower Bollinger Band', 'Predicted Funding Rate', 'Next Funding Rate', 'Funding Bar']
+
+num_months = 101
+
+
+def read(file):
+    df = pd.read_csv(file)
+    assert df.columns.equals(pd.Index(columns)), df.columns
+    assert df.shape[1] == 15, df.shape
+    df = df[['time', 'close', 'Global Money Supply']]
+    df['time'] = pd.to_datetime(df['time']).apply(lambda ts: ts.to_period(freq='M'))
+    df = df[df['time'] < pd.Period('2023-05')].tail(num_months)
+    df.set_index('time', inplace=True)
+    assert df.shape == (num_months, 2), df.shape
+    assert df.index[-1] == pd.Period('2023-04'), df.index[-1]
+    assert df.index[0] == pd.Period('2014-12'), df.index[0]
+    return df
+
+
+# def globalMoney(file):
+#     gm = pd.read_csv(file)
+#     assert gm.shape == (539, 15), gm.shape
+#     assert gm.columns.equals(pd.Index(columns)), gm.columns
+#     gm = gm[['time', 'Global Money Supply']]
+#     gm['time'] = pd.to_datetime(gm['time']).values.astype(dtype='datetime64[D]')
+#     gm.set_index('time', inplace=True)
+#     return gm
+#
+#
+# def nasdaq(file):
+#     n = pd.read_csv(file)
+#     assert n.shape == (462, 15), n.shape
+#     assert n.columns.equals(pd.Index(columns)), n.columns
+#     n = n[['time', 'close']]
+#     n['time'] = pd.to_datetime(n['time']).values.astype(dtype='datetime64[D]')
+#     n.set_index('time', inplace=True)
+#     return n
+
 
 if __name__ == '__main__':
-    btc = pd.read_csv('BTC-USD.csv')
-    assert btc.shape == (3183, 7), btc.shape
-    btc_columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
-    assert btc.columns.equals(pd.Index(btc_columns)), btc.columns
+    btc = read('COINBASE_BTCUSD, 1M.csv')
+    assert btc['close'][0] == 340, btc['close'][0]
+    assert btc['Global Money Supply'][0] == 74.37004131552007, btc['Global Money Supply'][0]
 
-    m2_US = pd.read_csv('US_M2.csv')
-    assert m2_US.shape == (539, 15), m2_US.shape
-    m2_columns = ['time', 'open', 'high', 'low', 'close', 'Global Money Supply', 'Volume',
-                  'Volume MA', 'RSI', 'RSI-based MA', 'Upper Bollinger Band',
-                  'Lower Bollinger Band', 'Predicted Funding Rate', 'Next Funding Rate', 'Funding Bar']
-    assert m2_US.columns.equals(pd.Index(m2_columns)), m2_US.columns
+    nasdaq = read('NASDAQ_DLY_NDX, 1M.csv')
+    assert nasdaq['close'][0] == 4236.28, nasdaq['close'][0]
 
-    btc = btc[['Date', 'Close']]
-    btc['Date'] = pd.to_datetime(btc['Date'])
-    btc.set_index('Date', inplace=True)
-    assert btc.index[0] == pd.Timestamp('2014-09-17'), btc.index[0]
-    assert btc.iloc[0].item() == 457.334015, btc.iloc[0].item()
+    # gm = gm.filter(items=btc.index, axis=0)
+    # btc = btc.filter(items=gm.index, axis=0)
+    # nasdaq = nasdaq.filter(items=gm.index, axis=0)
 
-    m2_US = m2_US[['time', 'close']]
-    m2_US['time'] = pd.to_datetime(m2_US['time']).values.astype(dtype='datetime64[D]')
-    m2_US = m2_US[m2_US['time'] >= btc.index[0]]
-    m2_US.set_index('time', inplace=True)
-    assert m2_US.index[0] == pd.Timestamp('2014-10-01'), m2_US.index[0]
-    assert m2_US.iloc[0].item() == 11551400000000, m2_US.iloc[0].item()
+    num_months = 103
+    # assert btc.shape[0] == num_months, btc.shape[0]
+    # assert gm.shape[0] == num_months, gm.shape[0]
+    # assert nasdaq.shape[0] == num_months, nasdaq.shape[0]
+    # assert gm.index[0] == pd.Timestamp('2014-10-01'), gm.index[0]
+    # assert gm.iloc[0].item() == 75.85531594272636, gm.iloc[0].item()
 
-    fig, axes = plt.subplots(nrows=2, ncols=1, sharex='col')
-    btc.plot(ax = axes[0])
-    m2_US.plot(ax= axes[1])
+    (corr, pv) = sp.pearsonr(btc['close'], btc['Global Money Supply'])
+    assert corr == 0.8619596247709039, corr
+    assert pv == 5.753006999330866e-31, pv
+
+    fig, axes = plt.subplots(nrows=3, ncols=1, sharex='col')
+    btc['close'].plot(ax=axes[0])
+    btc['Global Money Supply'].plot(ax=axes[1])
+    nasdaq['close'].plot(ax=axes[2])
     plt.show()
